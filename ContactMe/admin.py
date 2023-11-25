44e3911cc7ce4b9058261075f27cd83f175acc20
salar <127493852+swlrx08@users.noreply.github.com>
2023-11-26 00:50:17 +0330

    .

--- ContactMe/admin.py
+++ ContactMe/admin.py
@@ -5,7 +5,7 @@ from ContactMe import models
 
 class ContactMeAdmin(admin.ModelAdmin):
     list_filter = ['email']
-    list_display = ['email', 'message']
+    list_display = ['email', 'message', 'is_read']
 
 
 admin.site.register(models.ContactMe, ContactMeAdmin)
